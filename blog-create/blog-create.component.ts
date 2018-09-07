8d2a6e9e93abc56c91d07b0f47cea8c5060e3abb
aakash1393 <30972760+Aakash1393@users.noreply.github.com>
2018-09-07 14:13:49 +0530

    Add files via upload

--- blog-create/blog-create.component.ts
+++ blog-create/blog-create.component.ts
@@ -5,7 +5,9 @@ import { BlogHttpService } from '../blog-http.service';
 import {ToastaService} from 'ngx-toasta';
 import {ToastaConfig} from 'ngx-toasta';
 import {ToastOptions} from 'ngx-toasta';
-
+import {ToastData} from 'ngx-toasta';
+import {ToastaEvent} from 'ngx-toasta';
+import {ToastaEventType} from 'ngx-toasta';
 
 @Component({
   selector: 'app-blog-create',
@@ -18,7 +20,7 @@ export class BlogCreateComponent implements OnInit {
   // {
   //   this.toastr;
   // }
-  constructor(private blogHttpService:BlogHttpService,private toastaService:ToastaService, private toastyConfig: ToastaConfig, private ToastOptions: ToastOptions) { 
+  constructor(private blogHttpService:BlogHttpService,private toastaService:ToastaService, private toastyConfig: ToastaConfig, private toastOptions: ToastOptions) { 
     // Assign the selected theme name to the `theme` property of the instance of ToastaConfig. 
     // Possible values: default, bootstrap, material
     this.toastyConfig.theme = 'material';
@@ -29,35 +31,54 @@ export class BlogCreateComponent implements OnInit {
   public blogCategory:string;
   public possibleCategories=["Comedy","Drama","Action","Technology"];
 
-
+  private insertedToasts: number[] = [];
   ngOnInit() {
+    this.toastaService.events.subscribe((event: ToastaEvent) => {
+      if (event.type === ToastaEventType.ADD) {
+        let toast: ToastData = event.value;
+        this.insertedToasts.push(toast.id);
+      } else if (event.type === ToastaEventType.CLEAR_ALL) {
+        this.insertedToasts = [];
+      }
+    });
   }
 
-  // addToast() {
-    
-  //   this.toastaService.default('Hi there');
-    
-  //   var toastOptions:ToastOptions = {
-  //       title: "My title",
-  //       msg: "The message",
-  //       showClose: true,
-  //       timeout: 5000,
-  //       theme: 'default',
-  //       onAdd: (toast:ToastData) => {
-  //           console.log('Toast ' + toast.id + ' has been added!');
-  //       },
-  //       onRemove: function(toast:ToastData) {
-  //           console.log('Toast ' + toast.id + ' has been removed!');
-  //       }
-  //   };
-    
-    
-  //   this.toastaService.info(toastOptions);
-  //   this.toastaService.success(toastOptions);
-  //   this.toastaService.wait(toastOptions);
-  //   this.toastaService.error(toastOptions);
-  //   this.toastaService.warning(toastOptions);
-  // }
+  themes = [{
+    name: 'Default Theme',
+    code: 'default'
+  }];
+
+
+  types = [{
+    name: 'Default',
+    code: 'default',
+  }];
+
+  options = {
+    title: 'Toast It!',
+    msg: 'Mmmm, tasties...',
+    showClose: true,
+    timeout: 5000,
+    theme: this.themes[0].code,
+    type: this.types[0].code
+  };
+
+  newToast() {
+    let toastOptions: ToastOptions = {
+      title: this.options.title,
+      msg: this.options.msg,
+      showClose: this.options.showClose,
+      timeout: this.options.timeout,
+      theme: this.options.theme,
+      // position: this.options.position,
+      onAdd: (toast: ToastData) => {
+        console.log('Toast ' + toast.id + ' has been added!');
+      },
+      onRemove: function (toast: ToastData) {
+        console.log('Toast ' + toast.id + ' has been removed!');
+      }
+    };
+  }
   public createBlog(): any{
     let blogData ={
       title:this.blogTitle,
@@ -70,15 +91,17 @@ export class BlogCreateComponent implements OnInit {
     this.blogHttpService.createBlog(blogData).subscribe(
 
       data => {
+        // this.newToast();
         console.log("Blog Created");
         console.log(data);
-        this.toastaService.default({
-          title: "Toast It!",
-          msg: "Mmmm, tasties...",
-          showClose: true,
-          timeout: 1000,
-          theme: "default"
-      });
+      //   this.toastaService.default({
+      //     title: "Toast It!",
+      //     msg: "Mmmm, tasties...",
+      //     showClose: true,
+      //     timeout: 1000,
+      //     theme: "default"
+      // });
+       
     },
          
       error =>{
